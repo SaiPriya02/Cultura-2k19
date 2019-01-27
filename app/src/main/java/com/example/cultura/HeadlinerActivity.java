34dc26b6a1b8d920b366b2ed63987447a8c87f29
reallybadcode <dylanpinheiro98@gmail.com>
2019-01-27 22:00:37 +0530

    MainActivity created

--- app/src/main/java/com/example/cultura/HeadlinerActivity.java
+++ app/src/main/java/com/example/cultura/HeadlinerActivity.java
@@ -1,7 +1,9 @@
 package com.example.cultura;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class HeadlinerActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class HeadlinerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_headliner);
+
+
     }
 }
