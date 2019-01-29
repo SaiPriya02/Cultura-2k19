f587f43191edeb75ba610b4228b0a330c067200d
Kaiwalya2502 <kaiw17cs@cmrit.ac.in>
2019-01-29 18:33:03 +0530

    Merge branch 'dylan'

--- app/src/main/java/com/example/cultura/HeadlinerActivity.java
+++ app/src/main/java/com/example/cultura/HeadlinerActivity.java
@@ -1,8 +1,13 @@
 package com.example.cultura;
 
+
 import android.support.v4.view.ViewPager;
+
+import android.support.v7.app.ActionBar;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class HeadlinerActivity extends AppCompatActivity {
 
@@ -11,6 +16,7 @@ public class HeadlinerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_headliner);
+
         viewPager=findViewById(R.id.headlinerActivityViewPager);
 
         HeadlinerActivityAdapter headlinerActivityAdapter = new HeadlinerActivityAdapter(this);
