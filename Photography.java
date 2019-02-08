620ca7c0eee295d451306d4e79f194abc9bae935
Sai Priya <saipriyad2012@gmail.com>
2019-02-08 18:57:49 +0530

    Add files via upload

---
+++ Photography.java
@@ -0,0 +1,51 @@
+package com.example.cultura;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import java.util.ArrayList;
+
+public class Photography extends MainActivity implements Photo_Adapter.OnItemClickListener {
+
+    private RecyclerView pRecyclerView; //this is not being used wth
+    private Photo_Adapter pAdapter; //this is changed from RecyclerView.Adapter to use the custom methods that we made
+    private RecyclerView.LayoutManager pLayoutManager;
+    private Context pContext = this;
+    final ArrayList<PhotoItem> photoList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); //art_main is redundant
+
+        photoList.add(new PhotoItem("Photography Event 1"));
+        photoList.add(new PhotoItem("Photography Event 2"));
+        photoList.add(new PhotoItem("Photography Event 3"));
+        photoList.add(new PhotoItem("Photography Event 4"));
+        photoList.add(new PhotoItem("Photography Event 5"));
+        pRecyclerView = findViewById(R.id.recyclerView);
+        pRecyclerView.setHasFixedSize(true); //if the recyclerView won't change in size, no matter the content
+        pLayoutManager = new LinearLayoutManager(this);
+        pAdapter = new Photo_Adapter(photoList, this);
+        pRecyclerView.setLayoutManager(pLayoutManager);
+        pRecyclerView.setAdapter(pAdapter);
+
+      /*  pAdapter.setOnItemClickListener(new Photo_Adapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                photoList.get(position); //get(position).<call function here>
+                // Intent intent = new Intent(mContext, Art.class); //but this has to change depending on  which card is clicked //also change this
+                //   startActivity(intent);
+            }
+        }); */
+    }
+
+    @Override
+    public void onItemClick(int position) {
+      //  photoList.get(position);
+        Intent intent = new Intent(pContext, InfoActivity.class);
+        intent.putExtra("event name", "position of the event");
+        startActivity(intent);
+    }
+}
---
+++ Theatre.java
@@ -0,0 +1,52 @@
+package com.example.cultura;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import java.util.ArrayList;
+
+public class Theatre extends MainActivity implements Theatre_Adapter.OnItemClickListener {
+
+    private RecyclerView tRecyclerView; //this is not being used wth
+    private Theatre_Adapter tAdapter; //this is changed from RecyclerView.Adapter to use the custom methods that we made
+    private RecyclerView.LayoutManager tLayoutManager;
+    private Context tContext = this;
+    final ArrayList<TheatreItem> theatreList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); //art_main is redundant
+
+        theatreList.add(new TheatreItem("Theatre Event 1"));
+        theatreList.add(new TheatreItem("Theatre Event 2"));
+        theatreList.add(new TheatreItem("Theatre Event 3"));
+        theatreList.add(new TheatreItem("Theatre Event 4"));
+        theatreList.add(new TheatreItem("Theatre Event 5"));
+
+        tRecyclerView = findViewById(R.id.recyclerView);
+        tRecyclerView.setHasFixedSize(true); //if the recyclerView won't change in size, no matter the content
+        tLayoutManager = new LinearLayoutManager(this);
+        tAdapter = new Theatre_Adapter(theatreList, this);
+        tRecyclerView.setLayoutManager(tLayoutManager);
+        tRecyclerView.setAdapter(tAdapter);
+
+       /* tAdapter.setOnItemClickListener(new Theatre_Adapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                theatreList.get(position); //get(position).<call function here>
+                // Intent intent = new Intent(mContext, Art.class); //but this has to change depending on  which card is clicked //also change this
+                //   startActivity(intent);
+            }
+        }); */
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        theatreList.get(position);
+        Intent intent = new Intent(tContext, InfoActivity.class);
+        intent.putExtra("event name", "position of the event");
+        startActivity(intent);
+    }
+}
